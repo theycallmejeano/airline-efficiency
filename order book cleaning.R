2aa2de6d5c578f0e7757149d4f5395dc969514d9
theycallmejeano <jeangithae@outlook.com>
2026-01-05 15:46:08 +0000

    small updates

---
+++ order book cleaning.R
@@ -0,0 +1,172 @@
+library(tidyverse)
+library(fuzzyjoin)
+source("utilities.R")
+
+
+# --- STEP 1: combine Airbus and Boeing Order books -----
+# merge airbus data
+df_airbus<-merge_airbus_data("data/backlog data/airbus raw") %>%
+  mutate(Customer=ifelse(Customer=="Wizz Air Hungary", "Wizz Air", 
+                         ifelse(Customer == "Qantas Airways", "Qantas", Customer)))
+
+# clean up boeing orders
+boeing<- read_csv("data/boeing orders.csv") %>%
+  group_by(`Customer Name`, `Order Year`) %>%
+  summarise(`Total Backlog`=sum(`Unfilled Orders`))
+
+# merge order books
+merged_backlog<-df_airbus %>% full_join(boeing, by=c("Customer"="Customer Name",
+                                                     "Year"="Order Year")) %>%
+  group_by(Customer, Year) %>%
+  summarise(Total_Backlog = sum(Backlog, na.rm = TRUE) +
+              sum(`Total Backlog`, na.rm = TRUE),
+            .groups = "drop") %>% 
+  rename("Backlog"="Total_Backlog")
+
+# --- STEP 2: merge order backlogs to DEA data --------
+# filter for airline sample
+airline_sample<-read_csv("data/full data extended.csv")
+
+merged_input<- airline_sample %>% 
+       left_join(merged_backlog, 
+                 by=c("Airline"="Customer",
+                      "Year")) %>%
+  mutate(Year=as.numeric(Year),
+         DMU=paste(Airline, Year, sep = "_"))
+  #filter(!is.na(Backlog))
+
+# validations
+# 1. check that all airlines have all data for the entire period
+merged_input %>%
+  mutate(any_missing = if_any(everything(), is.na)) %>%
+  filter(any_missing) %>%
+  select(Airline, Year)
+
+# Ryanair(2018, 2019, 2021,2022, 2024) and Southwest(2020, 2022,2024) missing backlog data 
+
+# ----- using covid paper data -----
+test_data<-read_csv("data/poc data.txt") %>%
+  mutate(Airline=ifelse(Airline=="EasyJet","Easyjet",Airline))
+
+# merge to order books
+mock_data<- test_data %>%
+  mutate(Airline=ifelse(Airline=="Delta Airlines","Delta Air Lines",
+                        ifelse(Airline=="IndiGo", "Indigo", Airline))) %>%
+  left_join(merged_backlog, by=c("Airline"="Customer", "Year")) %>%
+  filter(Year>2018&!Airline%in%c("LATAM Airlines","Hainan Airline","Qantas airway",
+                                 "Scandinavian Airlines","Cathay Pacific Airways","	KLM")) %>%
+    mutate(Backlog=ifelse(Airline=="Ryanair"&Year %in%c(2018,2019,2021,2022),10, 
+                          ifelse(Airline=="Ryanair"&Year>2023, 150, Backlog)),
+           DMU=paste(Airline, Year, sep = "_")) %>%
+  rename("Fleet size"="Fleet_Size")
+
+# DEA tests
+mock_inputs<-c("Operating_expense", "Number_of_employees","Fleet_size")
+mock_outputs<-c("Total_revenue","RPK")
+run_window_dea_backlog(mock_data,mock_inputs, mock_outputs,1)
+
+
+# ---- metrics for report -----
+airlines <- c("Air Canada","Air France","American Airlines","British Airways",
+              "Delta Air Lines","Emirates","Lufthansa",
+              "Turkish Airlines","United Airlines","Qantas",
+              "Easyjet","Frontier Airlines","Indigo","Jetblue Airways",
+              "Ryanair","Spirit Airlines","South West Airlines","Wizz Air")
+
+merged_backlog %>% filter(Customer %in% airlines) %>% 
+  group_by(Customer) %>% 
+  summarise(sum(Backlog))
+
+
+# Select variables for median calculation
+vars <- c("Fleet size", "Passenger Load factor (%)", "Number of employees", 
+          "Revenue", "Expenses", "RPK", "ASK", "Backlog")
+
+median_df <- merged_input %>%
+  group_by(Year) %>%
+  summarise(across(all_of(vars), median, na.rm = TRUE)) %>%
+  pivot_longer(cols = all_of(vars), names_to = "Variable", values_to = "Median") %>%
+  mutate(Period = factor(
+    case_when(
+      Year %in% 2018:2019 ~ "Pre-pandemic",
+      Year %in% 2020:2021 ~ "Pandemic",
+      Year >= 2022        ~ "Pandemic recovery"
+    ),
+    levels = c("Pre-pandemic", "Pandemic", "Pandemic recovery")
+  ))
+
+# fig 4.1
+ggplot(median_df, aes(x = factor(Year), y = Median, fill=Period)) +
+  geom_col(width=0.6) +
+  facet_wrap(~Variable, scales = "free_y") +
+  scale_fill_manual(values = c(
+    "Pre-pandemic" = "#a6cee3",      # light blue
+    "Pandemic" = "#fb9a99",          # light red
+    "Pandemic recovery" = "#fdbf6f"  # light orange
+  )) +
+  labs(
+    title = "Median Values per Year",
+    x = "Year",
+    y = "Median",
+    fill = "Period"
+  ) +
+  theme_classic(base_size = 16) +
+  theme(
+    axis.text.x = element_text(hjust = 1),
+    strip.text = element_text(face="bold"))
+
+# who had bigger dip in lf
+merged_input_ext %>% 
+  select(Airline, Year, `Passenger Load factor (%)`) %>% 
+  filter(Year %in% c(2019, 2020)) %>%
+  left_join(efficiency_wbacklog_ext[,c("Airline", "Year", "Operating_Model")], by=c("Airline", "Year")) %>%
+  mutate(Operating_Model=ifelse(Airline=="Air Canada", "Hub_and_Spoke", Operating_Model)) %>%
+  group_by(Operating_Model, Year) %>% summarise(mean(`Passenger Load factor (%)`))
+
+merged_input_ext %>% 
+  select(Airline, Year, `RPK`) %>% 
+  filter(Year %in% c(2019, 2020)) %>%
+  left_join(efficiency_wbacklog_ext[,c("Airline", "Year", "Operating_Model")], by=c("Airline", "Year")) %>%
+  pivot_wider(id_cols=c("Airline", "Operating_Model"), names_from=Year, values_from=`RPK`) %>% 
+  mutate(point_change=`2020`-`2019`,
+         Operating_Model=ifelse(Airline=="Air Canada", "Hub_and_Spoke", Operating_Model)) %>%
+  group_by(Operating_Model) %>% summarise(mean(point_change))
+
+merged_input_ext %>% 
+  select(Airline, Year, RPK) %>% 
+  left_join(efficiency_wbacklog_ext[,c("Airline", "Year", "Operating_Model")], by=c("Airline", "Year")) %>%
+  mutate(Operating_Model=ifelse(Airline=="Air Canada", "Hub_and_Spoke", Operating_Model)) %>%
+  group_by(Operating_Model, Year) %>% summarise(`Mean RPK`=mean(RPK)) %>%
+  arrange(Operating_Model, Year) %>%
+  group_by(Operating_Model) %>%
+  mutate(
+    Delta_RPK = `Mean RPK` - lag(`Mean RPK`),
+    Delta_Year = paste0(Year, "–", Year - 1)
+  ) %>%
+  filter(!is.na(Delta_RPK)) %>%
+  ggplot(aes(x = Year, y = `Delta_RPK`, color=Operating_Model)) +
+  geom_hline(yintercept = 0, linetype = "dashed", colour = "grey60") +
+  geom_line(size = 1.2) +
+  geom_text(
+    aes(label = scales::comma(round(Delta_RPK))),
+    vjust = ifelse(Delta_RPK > 0, -0.6, 1.2),
+    size = 4,
+    show.legend = FALSE
+  ) +
+  scale_y_continuous(labels = scales::comma) +
+  scale_colour_manual(values = c(
+    "Hub and Spoke" = "#a6cee3",
+    "Point to Point" = "#fb9a99"
+  )) +
+  labs(
+    title = "Year-on-Year Change in RPKs by Operating Model",
+    x = "Year",
+    y = "Changes in average RPKs",
+    colour = "Operating model"
+  ) +
+  theme_classic(base_size = 16)
+  # theme(
+  #   axis.text.x = element_text(hjust = 1),
+  #   strip.text = element_text(face="bold"))
+
+                                                
---
+++ utilities.R
@@ -0,0 +1,209 @@
+# utilities
+library(tidyverse)
+library(readxl)
+library(zoo)
+
+# --- functions ----
+merge_airbus_data<-function(folder_path) {
+  # Function to read through the Airbus order book data from 2018 to 2024 and merge to a single dataframe 
+  # 1. Loop through folder: Get list of all excel files
+  filenames <- list.files(path = folder_path, pattern = "*.xlsx")
+  
+  # 2. Use map_df to merge the files
+  all_data <- map_df(filenames, function(filename) {
+    print(paste("Cleaning", filename))
+    
+    filepath <- file.path(folder_path, filename)
+    # read data
+    df <- read_excel(filepath, sheet = "Worldwide", skip = 17)
+    
+    raw_names <- colnames(df)
+    
+    # 2. "Fill" the aircraft names across the ...n columns
+    clean_names <- tibble(name = raw_names) %>%
+      mutate(name = ifelse(str_detect(name, "\\.\\.\\."), NA, name)) %>%
+      fill(name, .direction = "down") %>%
+      pull(name)
+    
+    # 3. Apply these filled names to the dataframe
+    # This turns [A330-200F, ...76, ...77] into [A330-200F_Ord_75, A330-200F_Del_76, A330-200F_Opr_77]
+    sub_headers <- as.character(df[1, ])
+    colnames(df) <- paste0(clean_names, "_", sub_headers, "_", seq_along(clean_names))
+    
+    # extract the dataset
+    df_clean<- df %>% 
+      select(2, matches("F_|TOTAL")) %>% # select the Total columns and cargo fleet columns
+      slice(-1) %>%  # drop the first row
+      pivot_longer(cols=2:last_col()) %>%
+      mutate(raw_type=str_split_i(name, "_", 2),
+             value=as.numeric(value),
+             type=ifelse(str_detect(name, "TOTAL"), paste("Total",raw_type), raw_type)) %>%
+      rename("Customer" = 1) %>% 
+      group_by(Customer, type) %>% summarise(sum=sum(value)) %>%
+      pivot_wider(id_cols=Customer, names_from="type", values_from="sum") %>%
+      # subtract the cargo fleet from total fleet, so that we get the passenger fleet numbers
+      mutate(Customer=str_to_title(Customer),
+             Deliveries=`Total Del`-coalesce(Del,0),
+             Orders=`Total Ord`-coalesce(Ord,0),
+             Operational=`Total Opr`-coalesce(Opr,0),
+             Backlog=Orders-Deliveries,
+             # get the year from the filename
+             Year = as.numeric(stringr::str_extract(filename, "\\d{4}"))) %>%
+      rename("Customer" = 1) %>% 
+      select(Customer, Year,Deliveries, Orders, Operational, Backlog) %>%
+      filter(!is.na(Customer))
+    
+    return(df_clean)
+    })
+
+  return(all_data)
+}
+
+# Function to run Window DEA
+run_dea <- function(data, input_list, output_list, orientation="oo", rts="vrs", stage="one") {
+
+  # General function to run DEA
+  # Args
+    # data: dataframe with inputs and outputs
+    # input_list: list of input cols
+    # output_list: list of output cols
+    # orientation : DEA orientation model input oriented(io) or output oriented(oo)
+    # rts : returns to scale model, variable returns to scale(vrs) or constant returns to scale(crs)
+    
+  # Create DEA data
+  input_idx  <- which(names(data) %in% input_list)
+  output_idx <- which(names(data) %in% output_list)
+    
+  tryCatch({
+    dea_data <- make_deadata(
+      data,
+      dmus = "DMU",  # DMU column position
+      inputs = input_idx, 
+      outputs = output_idx    
+    )
+     cat("Number of DMUs:", dea_data$n, "\n")
+     cat("Number of inputs:", dea_data$m, "\n")
+     cat("Number of outputs:", dea_data$s, "\n")
+    # 
+    # # Print actual data to check
+    # cat("\nInput data (first 5 rows):\n")
+    # print(head(dea_data$input, 5))
+    # print(head(dea_data$output, 5))
+    
+    # Run DEA
+    dea_result <- model_sbmeff(dea_data, orientation = orientation, rts = rts)
+    
+    #cat("  DEA completed with", length(efficiencies(dea_result)), "efficiency scores\n")
+    # print(efficiencies(dea_result))
+    
+    # Store results
+    results_list <- data.frame(
+      DMU = data$DMU,
+      Airline = data$Airline,
+      Year = data$Year,
+      Backlog = data$Backlog,
+      Fleet=data$`Fleet size`,
+      Efficiency = efficiencies(dea_result)
+    )
+    
+   error = function(e) {
+    cat("  ERROR:", e$message, "\n")
+    return(NULL)}
+  })
+  
+  bind_rows(results_list)
+}
+
+# function to add backlog metrics to input dataframe
+add_backlog_metrics<- function(dataframe){
+  # Step 1: Calculate persistent backlog metrics
+  backlog_metrics <- dataframe %>%
+    arrange(Airline, Year) %>%
+    group_by(Airline) %>%
+    mutate(
+      # Backlog severity (relative to fleet)
+      Backlog_Fleet_Ratio = (Backlog / Fleet) * 100,
+      
+      # Backlog growth
+      Backlog_Growth = (Backlog - lag(Backlog)) / lag(Backlog) * 100,
+      Backlog_Growth = ifelse(is.na(Backlog_Growth), 0, Backlog_Growth),
+      
+      # lagged values, lagging values as it takes ~2 years to get a new plane
+      Backlog_lag2 = lag(Backlog, 2),
+      Backlog_Growth_lag2 = lag(Backlog_Growth, 2),
+      
+      # rolling averages
+      Backlog_rollavg2 = rollmean(Backlog, k = 2, align = "right", fill = NA),
+    ) %>%
+    ungroup() %>%
+    mutate(
+      Operating_Model = case_when(
+        # TODO : remove from code
+        # Low-cost carriers (typically P2P)
+        Airline %in% c("Easyjet", "Ryanair", "Indigo", "Wizz Air", "Frontier Airlines", "Jetblue Airways",
+                       "Southwest Airlines", "Spirit Airlines") ~ "Point_to_Point",
+        
+        # Major network carriers (hub-and-spoke)
+        Airline %in% c("American Airlines", "United Airlines", "Delta Air Lines",
+                       "Lufthansa", "British Airways", "Emirates", "Turkish Airlines",
+                       "Korean Air", "Singapore Airlines", "Qantas",
+                       "China Southern Airlines", "China Eastern Airlines",
+                       "Air Canada") ~ "Hub_and_Spoke",
+        TRUE ~ "Other"),
+      Period=case_when(Year %in% c(2018, 2019)~"Pre-pandemic",
+                       Year %in% c(2020, 2021) ~ "Pandemic",
+                       TRUE ~ "Pandemic recovery"))
+  
+  return(backlog_metrics)
+}
+
+# weighting
+normalize_data <- function(data) {
+  data_norm <- data %>%
+    mutate(
+      # Normalize by dividing by maximum
+      fleet_size_norm = `Fleet size` / max(`Fleet size`),
+      opex_norm = Expenses / max(Expenses),
+      ask_norm = ASK / max(ASK),
+      employees_norm = `Number of employees` / max(`Number of employees`),
+      revenue_norm = Revenue / max(Revenue),
+      load_factor_norm = `Passenger Load factor (%)` / max(`Passenger Load factor (%)`)
+    )
+  
+  return(data_norm)
+}
+
+calculate_weights <- function(data) {
+  data <- data %>%
+    mutate(
+      # Aggregate Stage 1 output (normalized ASK)
+      Z0 = ask_norm,
+      
+      # Aggregate Stage 2 outputs (normalized RPK + Load Factor)
+      Y0 = revenue_norm + load_factor_norm,
+      
+      # Total aggregate output
+      total_output = Z0 + Y0,
+      
+      # Calculate weights
+      w1 = Z0 / total_output,
+      w2 = Y0 / total_output
+    )
+  
+  return(data)
+}
+
+rank_scores <- function(data, score_col="Additive_Efficiency"){
+  ranked_data <- data %>%
+    group_by(Year) %>%
+    mutate(
+      # Rank (1 = most efficient)
+      Efficiency_Rank = rank(-get(score_col), ties.method = "min")) %>%
+    ungroup() %>%
+    select(Airline, Year, Operating_Model, Efficiency_Rank) %>%
+    pivot_wider(id_cols=c("Airline", "Operating_Model"),
+                names_from=Year,
+                values_from = "Efficiency_Rank")
+  
+  return(ranked_data)
+}
